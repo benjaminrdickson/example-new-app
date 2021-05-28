c95e55f49a2ddfd5b098493465d928b06f557ffd
benjaminrdickson <bdickson8619@gmail.com>
2021-05-28 14:28:25 -0500

    put the correct stored variable into controller code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
 
   def index
-    response = HTTP.headers("X-Api-Key" => "SOMEKEY").get("https://newsapi.org/v2/everything?q=#{params[:search]}")
+    response = HTTP.headers("X-Api-Key" => Rails.application.credentials.some_api_key).get("https://newsapi.org/v2/everything?q=#{params[:search]}")
     render json: response.parse(:json)["articles"]
   end
-
+  
 
 end
